ec1b5aca1d7ff387e6415103fcfdb0041b9da0ff
asebastian36 <asebastianjob@gmail.com>
2023-09-30 11:31:51 -0600

    avances con el proyecto de metodos numericos, ya se importaron algunas de las dependencias necesarias y se agrego una clase de prueba

---
+++ metodosnumericos/src/main/java/com/metodosnumericos/view/FunctionGraphExample.java
@@ -0,0 +1,31 @@
+package com.metodosnumericos.view;
+
+import javax.swing.JFrame;
+import org.jfree.chart.*;
+import org.jfree.data.xy.*;
+
+public class FunctionGraphExample {
+    public static void main(String[] args) {
+        // Crea una serie de datos.
+        XYSeries series = new XYSeries("f(x) = x^2 - 100");
+        for (double x = -10; x <= 10; x += 0.1) {
+            series.add(x, x * x - 100);
+        }
+
+        // Crea una colección de series de datos.
+        XYSeriesCollection collection = new XYSeriesCollection();
+        collection.addSeries(series);
+
+        // Crea un gráfico.
+        JFreeChart chart = ChartFactory.createXYLineChart("f(x) = x^2 - 100", "x", "y", collection);
+
+        // Crea un panel para el gráfico.
+        ChartPanel panel = new ChartPanel(chart);
+
+        // Muestra el gráfico.
+        JFrame frame = new JFrame("f(x) = x^2 - 100");
+        frame.add(panel);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+}
